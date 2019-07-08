c933590f26d5076833482d8e18c6a7902bf85d7b
Sandro <sandro.bjelogrlic@gmail.com>
2019-07-08 15:19:56 +0200

    #1 modified test for simplicity

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,5 +1,9 @@
 import os.path
 
-# Do you appreciate circular logic?
-def test_dummy_exists():
-    assert os.path.isfile('test_dummy.py') == True
+# # Do you appreciate circular logic?
+# def test_dummy_exists():
+#     assert os.path.isfile('test_dummy.py') == True
+
+
+def test_sum():
+    assert 2+2==4
