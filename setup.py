468de65e46d796b80d0b79a1159ef650aac7e399
Frits <frits.hermans@ing.com>
2019-07-09 13:53:11 +0200

    updates .gitlib-ci.yml to have automatic pip deployment

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 try:
     if os.environ.get('CI_COMMIT_TAG'):
-    	version = os.environ['CI_COMMIT_TAG']
+        version = os.environ['CI_COMMIT_TAG']
     else:
         version = os.environ['CI_JOB_ID']
 except:
