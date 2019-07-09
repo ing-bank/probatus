4925f94b72e7993a789fed7232c770a6aaba437e
Sandro <sandro.bjelogrlic@gmail.com>
2019-07-09 15:04:02 +0200

    #28 added base package and local version

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 import setuptools
 import os
 
+
+
+base_packages = ["numpy>=1.15.4", "scipy>=1.2.0", "scikit-learn>=0.20.2",
+                 "pandas>=0.23.4"]
+
 try:
     if os.environ.get('CI_COMMIT_TAG'):
     	version = os.environ['CI_COMMIT_TAG']
     else:
         version = os.environ['CI_JOB_ID']
 except:
-    version = 'local-5'
+    version = 'local'
 
 
 setuptools.setup(
@@ -19,6 +24,7 @@ setuptools.setup(
     license='ING Open Source',
     packages=setuptools.find_packages(),
     package_data={'pyrisk': ['datasets/data/*.pkl']},
+    install_requires=base_packages,
     url='https://gitlab.com/ing_rpaa/pyrisk',
     zip_safe=False
 )
