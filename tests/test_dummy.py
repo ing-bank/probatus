96045a1c0871e694b3e5ecca15141a1ccd8e3644
Frits <frits.hermans@ing.com>
2019-07-09 13:17:38 +0200

    first

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,9 +1,10 @@
 import os.path
 
+
 # # Do you appreciate circular logic?
 # def test_dummy_exists():
 #     assert os.path.isfile('test_dummy.py') == True
 
 
 def test_sum():
-    assert 2+2==4
+    assert 2 + 2 == 4
