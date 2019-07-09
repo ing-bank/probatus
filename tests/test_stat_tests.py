d3978d5786131e93500ef11f2a74aacf048296de
Sandro <sandro.bjelogrlic@gmail.com>
2019-07-09 16:44:37 +0200

    #4 refactored the tests to be submodule compliant

--- tests/test_stat_tests.py
+++ tests/test_stat_tests.py
@@ -1,5 +1,17 @@
 import numpy as np
+from pyrisk.stat_tests.psi import psi
 from pyrisk.stat_tests.ks import ks
+
+def test_psi_returns_zero():
+    d1 = np.random.normal(size = 1000)
+    d2 = d1
+    assert psi(d1, d2) == 0.0
+
+def test_psi_returns_large():
+    d1 = np.random.normal(size = 1000)
+    d2 = np.random.weibull(1, size = 1000) - 1
+    assert psi(d1, d2) > 1.0
+
 
 
 def test_ks_returns_one():
