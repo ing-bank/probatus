4aa400b3f95e5a86a0c7bba6565a910c71b8d7d3
Sandro <sandro.bjelogrlic@gmail.com>
2019-08-07 13:16:13 +0200

    #14 Added docstring and fixed bug with shapes in case of lr model

--- pyrisk/samples/model.py
+++ pyrisk/samples/model.py
@@ -15,10 +15,32 @@ class ResemblanceModel(object):
 
     def __init__(self, model_type='rf', keep_samples=False, **model_kwargs):
         """
+        This model checks for similarity of two samples.
+
+        It assignes to each sample a label, reshuffles the values and randomly selects a fraction of the sample mix for
+        training the model, and the remainder to estimate how well it can distinguish the samples.
+
+        If the samples are coming from the same distribution, you would expect an AUC on the test to be of the order
+        of 0.5, i.e. it is not possible to distingusih the samples
 
         Args:
-            model_type:
-            keep_samples:
+            model_type: (str or model) define which model you want to run for resemblance modeling.
+            Currently supported:
+                - 'rf' (default): RandomForestClassifier from sklearn
+                - 'lr': LogisticRegression from sklearn
+
+                You can define your custom model object:
+                Example:
+                    from xgboost import XGBoostClassifier
+
+                    ResemblanceModel(model_type=XGBoostClassifier()).fit(X1,X2)
+
+            keep_samples: boolean, default is False. Set it to True if you want to retrieve the samples used by
+                the underlying model to fit. Those samples are build from merging the X1 and X2 samples and then randomly
+                sampling.
+                If true, the samples are available as properties:
+                    - X_train: features used for training the underlying model
+                    - y_train: label of the samples used
             **model_kwargs:
         """
 
@@ -32,13 +54,13 @@ class ResemblanceModel(object):
         elif model_type =='lr':
             self.model = LogisticRegression(**model_kwargs)
         else:
-            self.model = model_type
+            self.model = model_type(**model_kwargs)
 
 
     def __repr__(self):
-        repr_ = f"{self.__class__.__name__}\n\tModel type: {self.get_model_name()}"
+        repr_ = f"{self.__class__.__name__}\n\tUnderlying model type: {self.get_model_name()}"
         if self.fitted:
-            repr_ += "\nThe model predicts the samples with an AUC of {:.3f}".format(self.auc_test)
+            repr_ += "\nThe model is able to distinguish the samples with an AUC of {:.3f}".format(self.auc_test)
         return repr_
 
     def get_model_name(self):
@@ -57,10 +79,12 @@ class ResemblanceModel(object):
         except:
             try:
                 importances = self.model.coef_
+                importances = importances.reshape(importances.shape[1],)
+
             except:
                 raise UnsupportedModelError("Model type {} is not supported".format(self.get_model_name()))
 
-
+        print(importances.shape)
         if self.col_names is not None:
             feat_importances = pd.Series(importances, index=self.col_names)
         else:
@@ -95,15 +119,21 @@ class ResemblanceModel(object):
         """Calculates and returns the AUC score.
 
         Args:
-            X1 (numpy.ndarray)          : first sample
-            X2 (numpy.ndarray)          : second sample
+            X1 (numpy.ndarray): first sample
+            X2 (numpy.ndarray): second sample
+            test_size: size of the test sample (fraction)
+
 
         Returns:
-            AUC score                   : float
+            auc score on the training sample, auc on the test sample, and feature imporatances  : float
         """
-        col_names=None
+
+        assert X1.shape[1] == X2.shape[1], "The number of columns in X1 and X2 does not match!"
+        assert type(X1) == type(X2), "Non matching types for  X1 and X2"
+
         if type(X1)==pd.DataFrame:
             self.col_names = X1.columns
+            assert X1.columns.tolist() == X2.columns.tolist(), "The columns of the DataFrame X1 and X2 do not match"
 
         X1 = assure_numpy_array(X1)
         X2 = assure_numpy_array(X2)
@@ -133,9 +163,22 @@ class ResemblanceModel(object):
 
         return auc_train, auc_test, importances
 
-    def fit(self, X1,X2):
+    def fit(self, X1,X2, **kwargs):
+        """
+            Fit the underlying model to compare the samples X1 or X2
 
-        self.auc_train, self.auc_test, self.importances = self._propensity_check(X1,X2)
+
+        Args:
+            X1 (numpy.ndarray or pd.DataFrame): first sample
+            X2 (numpy.ndarray or pd.DataFrame): second sample
+            **kwargs:
+                test_size (float, default=0.33): fraction to use for test samples
+                random_state=None: random state of train test split
+
+        """
+
+
+        self.auc_train, self.auc_test, self.importances = self._propensity_check(X1,X2,**kwargs)
         self.fitted=True
 
         return self
