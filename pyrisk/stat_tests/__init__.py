ccdc369347860da4c933a22fd495ff763c15acac
Frits <frits.hermans@ing.com>
2019-08-28 17:00:16 +0200

    adds AutoDist to list of __all__ in __init__.py in stat_tests

--- pyrisk/stat_tests/__init__.py
+++ pyrisk/stat_tests/__init__.py
@@ -2,6 +2,6 @@ from .ad import ad
 from .es import es
 from .ks import ks
 from .psi import psi
-from .distribution_statistics import DistributionStatistics
+from .distribution_statistics import DistributionStatistics, AutoDist
 
-__all__ = ['ad', 'es', 'ks', 'psi', 'DistributionStatistics']
+__all__ = ['ad', 'es', 'ks', 'psi', 'DistributionStatistics', 'AutoDist']
