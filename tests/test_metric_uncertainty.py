60bd4e346653de285a71b25f0bbd0bb40a52c2e2
Tim Vink <vinktim@gmail.com>
2019-12-10 08:58:41 +0000

    Replace pickled model with untrained RF model

--- tests/test_metric_uncertainty.py
+++ tests/test_metric_uncertainty.py
@@ -3,15 +3,20 @@ import numpy as np
 from probatus.metric_uncertainty import VolatilityEstimation
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import accuracy_score
-from probatus.models import lending_club_model
 from probatus.datasets import lending_club
+from sklearn.ensemble import RandomForestClassifier
 
 #### Fixtures 
 
 @pytest.fixture
 def model():
     # Return a dummy model
-    return lending_club_model()
+    return RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+        max_depth=6, max_features='auto', max_leaf_nodes=None,
+        min_impurity_decrease=0.0, min_impurity_split=None,
+        min_samples_leaf=1, min_samples_split=2,
+        min_weight_fraction_leaf=0.0, n_estimators=100, n_jobs=None,
+        oob_score=False, random_state=0, verbose=0, warm_start=False)
 
 @pytest.fixture
 def club_data():
