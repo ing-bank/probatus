57a853ed9ae63cd53ac813ba02388c79693b9bd4
Gilles Verbockhaven <gilles.verbockhaven.com>
2019-12-10 12:08:59 +0100

    Change to account for Shap 32 and higher (with changes in setup.py) and remove reading the model from pickle table because pkl model was not build with the latest version of sklearn

--- tests/test_inspector.py
+++ tests/test_inspector.py
@@ -28,8 +28,9 @@ def test_inspector():
 
     assert report.shape == (4, 6)
 
-    expected_confusion = np.array([0.43190657, 0.06716497, 0.0319691, 0.18831297])
-
+    #expected_confusion = np.array([0.43190657, 0.06716497, 0.0319691, 0.18831297])
+    expected_confusion = np.array([0.21282713, 0.08869656, 0.56882355, 0.02859485])
+    
     # The order might change - check the  sum of the values
     assert (np.abs((report["average_confusion"].values.sum() - expected_confusion.sum())) < 0.05)
 
--- tests/test_shapley_helpers.py
+++ tests/test_shapley_helpers.py
@@ -19,10 +19,14 @@ def get_feats_and_model():
 
 def get_shap_averages():
     # shapley values expected
-    abs_avg =  [0.0024, 0.0049, 0.0048, 0.0033, 0.0008, 0.0018, 0.0015, 0.0012, 0.0016, 0.0013, 0.0027, 0.0005,
-                0.0003, 0.0001, 0.0]
-    avg = [0.0023, 0.0005, -0.0001, -0.0033, -0.0005, 0.0007, 0.001, 0.0001, 0.0002, -0.0, 0.0011, -0.0005, -0.0002, -0.0,
-     -0.0]
+    avg = [0.00117, -0.00087, 0.0003, -0.0029, -0.0005, 0.00064, 0.0013, 0.0007, 2.12e-05, -8.5e-06, 0.00115, 
+           -0.0008, -0.00036, 7.5e-06, -4.6e-06]
+    abs_avg = [0.00149, 0.0034, 0.0033, 0.0029, 0.00113, 0.0015, 0.00198, 0.0013, 0.0012, 0.0011, 0.0023, 0.0008, 
+               0.00036, 0.00012, 8.2e-06]
+    #abs_avg =  [0.0024, 0.0049, 0.0048, 0.0033, 0.0008, 0.0018, 0.0015, 0.0012, 0.0016, 0.0013, 0.0027, 0.0005,
+    #            0.0003, 0.0001, 0.0]
+    #avg = [0.0023, 0.0005, -0.0001, -0.0033, -0.0005, 0.0007, 0.001, 0.0001, 0.0002, -0.0, 0.0011, -0.0005, -0.0002, -0.0,
+    # -0.0]
 
     return avg, abs_avg
 
@@ -32,14 +36,14 @@ def test_model_support():
     rf, X = get_feats_and_model()
 
 
-    shap_out = shap_help.shap_calc(rf, X, approximate=True)
+    shap_out = shap_help.shap_calc(rf, X, approximate=True, check_additivity=False)
     assert isinstance(shap_out, np.ndarray)
 
 
     #make sure that if you pass an unssupported model, the model raises the exceptions
     lr = LogisticRegression()
     with pytest.raises(UnsupportedModelError):
-        assert shap_help.shap_calc(lr, X, approximate=True)
+        assert shap_help.shap_calc(lr, X, approximate=True, check_additivity=False)
 
 
 
