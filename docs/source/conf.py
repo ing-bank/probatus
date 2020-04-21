16f3410fd7bfe859f25dd625eb3f813584fd1f0f
uj26kf <mateusz.garbacz@ing.com>
2020-04-21 15:40:26 +0200

    Hotfix release version in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'RPAA'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
