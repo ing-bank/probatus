3f218ba116459b414343338ed62381a8a887ed96
WE56SC <sandro.bjelogrlic@ing.com>
2020-07-23 17:44:30 +0200

    #159 added comment in test to understand better what it is doing"

--- tests/utils/test_utils_tree.py
+++ tests/utils/test_utils_tree.py
@@ -18,8 +18,14 @@ class mock_tree():
         self.tree_ = mock_sub_tree(node_count, cl, cr, feat, thresh)
 
 
-
 def test_simple_tree():
+    # Quick interpretation tip:
+    # The sklearn trees structure is represented by the arrays described in the mock tree.
+    # When the index is negative, this means that the leaf is a terminal leaf, otherwise is part of the node
+    # children left/right contain the index of the next leaf, while feat contains the index of the feature used.
+    # In this toy case, the leaf are at index 1,4,5,6 (corresponding to the negative indexes in the arrays).
+    # Furthermore, the order of the expected boundaries can be extracted from the thesholds
+
     test_tree_1 = mock_tree(
         node_count=7,
         cl=np.array([1, -1, 3, 4, -1, -1, -1]),
@@ -30,10 +36,12 @@ def test_simple_tree():
 
     tpf1 = TreePathFinder(test_tree_1)
 
+
     exp_boundaries = {1: {'min': -np.inf, 'max': -3.54},
                       4: {'min': -3.54, 'max': -1.985},
                       5: {'min': -1.985, 'max': 2.795},
                       6: {'min': 2.795, 'max': np.inf}}
+
 
     assert tpf1.get_boundaries() == exp_boundaries
     assert all(tpf1.is_leaves[[1,4,5,6]])#
