89ce678ae495998c5299ed94dcd58eea08e4feff
mgarbacz <mateusz.garbacz@ing.com>
2020-10-15 13:43:25 +0200

    Update year of copyrights

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 
 project = 'probatus'
-copyright = '2019, ING Risk and Pricing Advanced Analytics'
+copyright = '2020, ING Risk and Pricing Advanced Analytics'
 author = 'ING RPAA'
 
 # The short X.Y version
