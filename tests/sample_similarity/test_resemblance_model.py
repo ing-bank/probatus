f9cc77f002f72c053202ac1281482cfc1cea8aea
anilkumar.panda <panda.anil1989@gmail.com>
2021-02-17 10:18:15 +0100

    Updated tests with linear resemblance models.

--- tests/sample_similarity/test_resemblance_model.py
+++ tests/sample_similarity/test_resemblance_model.py
@@ -5,6 +5,7 @@ import pytest
 import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
 import os
 from pandas.api.types import is_numeric_dtype
 import matplotlib.pyplot as plt
@@ -29,7 +30,44 @@ def X2():
 
 
 def test_base_class(X1, X2):
-    clf = DecisionTreeClassifier(max_depth=1, random_state=1)
+    clf= DecisionTreeClassifier(max_depth=1, random_state=1)
+    rm = BaseResemblanceModel(clf, test_prc=0.5, n_jobs=1, random_state=42)
+
+    # Before fit it should raise an exception
+    with pytest.raises(NotFittedError) as _:
+        rm._check_if_fitted()
+
+    actual_report, train_score, test_score = rm.fit_compute(X1, X2, return_scores=True)
+
+    # After the fit this should not raise any error
+    rm._check_if_fitted()
+
+    assert train_score == 1
+    assert test_score == 1
+    assert actual_report is None
+
+    # Check data splits if correct
+    actual_X_train, actual_X_test, actual_y_train, actual_y_test = rm.get_data_splits()
+
+    assert actual_X_train.shape == (4, 3)
+    assert actual_X_test.shape == (4, 3)
+    assert len(actual_y_train) == 4
+    assert len(actual_y_test) == 4
+
+    # Check if stratified
+    assert np.sum(actual_y_train) == 2
+    assert np.sum(actual_y_test) == 2
+
+    # Check if index is correct
+    assert len(rm.X.index.unique()) == 8
+    assert list(rm.X.index) == list(rm.y.index)
+
+    with pytest.raises(NotImplementedError) as _:
+        rm.plot()
+
+def test_base_class_lin_models(X1, X2):
+    #Test class BaseResemblanceModel for linear models.
+    clf = LogisticRegression()
     rm = BaseResemblanceModel(clf, test_prc=0.5, n_jobs=1, random_state=42)
 
     # Before fit it should raise an exception
@@ -100,6 +138,41 @@ def test_shap_resemblance_class(X1, X2):
     rm.plot(plot_type='bar')
     rm.plot(plot_type='dot')
 
+def test_shap_resemblance_class_lin_models(X1, X2):
+    #Test SHAP Resemblance Model for linear models.
+    clf = LogisticRegression()
+    rm = SHAPImportanceResemblance(clf, test_prc=0.5, n_jobs=1, random_state=42)
+
+    # Before fit it should raise an exception
+    with pytest.raises(NotFittedError) as _:
+        rm._check_if_fitted()
+
+    actual_report, train_score, test_score = rm.fit_compute(X1, X2, return_scores=True)
+
+    # After the fit this should not raise any error
+    rm._check_if_fitted()
+
+    assert train_score == 1
+    assert test_score == 1
+
+    # Check report shape
+    assert actual_report.shape == (3, 2)
+    # Check if it is sorted by importance
+    assert actual_report.iloc[0].name == 'col_1'
+    # Check report values
+    assert actual_report.loc['col_1']['mean_abs_shap_value'] > 0
+    assert actual_report.loc['col_1']['mean_shap_value'] > 0
+    assert actual_report.loc['col_2']['mean_abs_shap_value'] == 0
+    assert actual_report.loc['col_2']['mean_shap_value'] == 0
+    assert actual_report.loc['col_3']['mean_abs_shap_value'] == 0
+    assert actual_report.loc['col_3']['mean_shap_value'] == 0
+
+    actual_shap_values_test = rm.get_shap_values()
+    assert actual_shap_values_test.shape == (4, 3)
+
+    # Run plots
+    rm.plot(plot_type='bar')
+    rm.plot(plot_type='dot')
 
 @pytest.mark.skipif(os.environ.get("SKIP_LIGHTGBM") == 'true', reason="LightGBM tests disabled")
 def test_shap_resemblance_class(complex_data, complex_lightgbm):
