57edb3f6143e7bcaf34b97e2a0b5e91ae9466fa9
anilkumar.panda <panda.anil1989@gmail.com>
2021-03-02 20:28:56 +0100

    Added support for RF,LR models in a elegant way.

--- probatus/missing/imputation.py
+++ probatus/missing/imputation.py
@@ -17,26 +17,31 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from probatus.utils import BaseFitComputeClass,BaseFitComputePlotClass
+from probatus.utils import preprocess_data, preprocess_labels,BaseFitComputePlotClass
 from  sklearn.model_selection import cross_val_score
-from  sklearn.pipeline import make_pipeline,Pipeline
+from  sklearn.pipeline import Pipeline
 from  sklearn.impute import SimpleImputer
 from  sklearn.compose import ColumnTransformer
 from  sklearn.preprocessing import OneHotEncoder
 import matplotlib.pyplot as plt
-
 import numpy as np 
-class CompareImputationStrategies(BaseFitComputeClass):
-    """
-    Comparison of various imputation stragegies
-    that can be used for imputation of missing values. 
+import pandas as pd 
 
-    Args :
+class CompareImputationStrategies(BaseFitComputePlotClass):
+    """
+    Comparison of various imputation stragegies that can be used for imputation 
+    of missing values. 
+    The aim of this class is to present the user the model performance is
+    based on the choosen metric and imputation strategy.
+    For models like XGBoost & LighGBM which have capabilities to handle misisng values, any
+    data transformation is not required.
+    However in the case of RandomForestClassifier,LogisticRegression 
+    the data must be transformed before passing for comparision.
 
     """
-    def __init__(self,clf,strategies,scoring='roc_auc',cv=5,verbose=0):
+    def __init__(self,clf,strategies,scoring='roc_auc',cv=5,model_na_support=True,verbose=0):
         """
-        Initialise the class 
+        Initialise the class.
 
         Args :
             clf(model object):
@@ -49,7 +54,16 @@ class CompareImputationStrategies(BaseFitComputeClass):
 
             strategies (dictionary of sklearn.impute objects):
                 Dictionary containing the sklearn.impute objects.
-                #TODO Add more documentation.
+                e.g.
+                
+                strategies = {'KNN' : KNNImputer(n_neighbors=3),
+                'Simple Median Imputer' : SimpleImputer(strategy='median',add_indicator=True),
+                'Iterative Imputer'  : IterativeImputer(add_indicator=True,n_nearest_features=5,
+                sample_posterior=True)}
+                This allows you to have fine grained control over the imputation method.
+            model_na_support(boolean): default True
+                If the classifier supports missing values by default e.g. LightGBM,XGBoost etc. If True an default 
+                comparison will be added without any imputation. If False only the provided strategies will be used.
 
             verbose (int, optional):
                 Controls verbosity of the output:
@@ -60,15 +74,21 @@ class CompareImputationStrategies(BaseFitComputeClass):
                 - above 100 - presents all prints and all warnings (including SHAP warnings).
         """
         self.clf = clf
+        self.model_na_support = model_na_support
         self.scoring = scoring
         self.strategies = strategies
         self.cv = cv
         self.verbose = verbose
-        self.results = {}
-        
+        self.fitted = False
+        self.results_df = None
+
+    def __repr__(self):
+        return "Imputation comparision for {}".format(self.clf.__class__.__name__)
+
+
     def fit(self, X, y,column_names=None,class_names=None,categorical_columns='auto'):
         """
-        Calculates score
+        Calculates the cross validated results for various imputation strategies.
         
         Args:
             X (pd.DataFrame):
@@ -78,17 +98,27 @@ class CompareImputationStrategies(BaseFitComputeClass):
                 target variable.
 
             column_names (None, or list of str, optional):
-                List of feature names for the dataset. If None, then column names from the X_train dataframe are used.
+                List of feature names for the dataset. 
+                If None, then column names from the X dataframe are used.
 
             class_names (None, or list of str, optional):
-                List of class names e.g. ['neg', 'pos']. If none, the default ['Negative Class', 'Positive Class'] are
+                List of class names e.g. ['neg', 'pos']. 
+                If none, the default ['Negative Class', 'Positive Class'] are
                 used.
             
-            categorical_features ((None, or list of str, optional):deafault=auto
-                List of categorical features to consider.
-                The imputation strategy for categorical is different
-                that compared to numerical features.
+            categorical_features (None, or list of str, optional):default=auto
+                List of categorical features.The imputation strategy for categorical 
+                is different that compared to numerical features. If auto try to infer
+                the categorical columns based on 'object' and 'category' datatypes.
         """
+        #Place holder for results.
+        results = []
+
+        self.X, self.column_names = preprocess_data(X, column_names=column_names,
+                                                          verbose=self.verbose)
+        self.y = preprocess_labels(y, index=self.X.index, verbose=self.verbose)
+                                                         
+
         #Identify categorical features if not explicitly specified.
         if 'auto' in categorical_columns:
             X_cat = X.select_dtypes(include=['category','object'])
@@ -97,129 +127,146 @@ class CompareImputationStrategies(BaseFitComputeClass):
                 X[column] = X[column].astype('category')
         else :
             #Check if the passed columns are in the dataframe.
+            assert categorical_columns in X.columns,"All categorical columns not in the dataframe."
             X_cat = X[categorical_columns]
-        
+        #Identify the numeric columns.Numeric columns are all columns expect the categorical
+        # columns
         X_num = X.drop(columns = categorical_columns,inplace=False)
         numeric_columns = X_num.columns.to_list()
-
-        #Add the No imputation to strategy.
-        self.strategies['No Imputation'] = None 
-
+        
         for strategy in self.strategies:
 
-            if 'No Imputation' in strategy:
-                
-                categorical_transformer = Pipeline(steps=[
-                    ('ohe_cat',OneHotEncoder(handle_unknown='ignore')),
-                ])
-
-                preprocessor = ColumnTransformer(
-                    transformers=[
-                        ('cat', categorical_transformer, categorical_columns)],
-                        remainder='passthrough')
-
-                self.clf = Pipeline(steps=[('preprocessor', preprocessor),
-                                    ('classifier', self.clf)])
-
-                imputation_results = cross_val_score(
-                self.clf,
-                X,
-                y,
-                scoring=self.scoring,
-                cv=self.cv)
-
-                self.results[strategy] = imputation_results
-                
-            else :
-
-                numeric_transformer = Pipeline(steps=[
+            numeric_transformer = Pipeline(steps=[
                     ('imputer', self.strategies[strategy])])
 
-                categorical_transformer = Pipeline(steps=[
+            categorical_transformer = Pipeline(steps=[
                     ('imp_cat',SimpleImputer(strategy='most_frequent',add_indicator=True)),
                     ('ohe_cat',OneHotEncoder(handle_unknown='ignore')),
                 ])
 
-                preprocessor = ColumnTransformer(
+            preprocessor = ColumnTransformer(
                     transformers=[
                         ('num', numeric_transformer, numeric_columns),
                         ('cat', categorical_transformer, categorical_columns)],
                         remainder='passthrough')
 
-                clf = Pipeline(steps=[('preprocessor', preprocessor),
+            clf = Pipeline(steps=[('preprocessor', preprocessor),
                                     ('classifier', self.clf)])
                 
-                imputation_results = cross_val_score(
+            imputation_results = cross_val_score(
                 clf,
                 X,
                 y,
                 scoring=self.scoring,
                 cv=self.cv)
 
-                self.results[strategy] = imputation_results
+            temp_results = {
+                    'strategy' : strategy,
+                    'score': np.round(np.mean(imputation_results),3),
+                    'std':np.round(np.std(imputation_results),3),
+                }
+            results.append(temp_results)
+        #If model supports missing values by default, then calculate the scores 
+        #on raw data without any imputation. 
+        if self.model_na_support :
+            categorical_transformer = Pipeline(steps=[
+                    ('ohe_cat',OneHotEncoder(handle_unknown='ignore')),
+                ])
 
+            preprocessor = ColumnTransformer(
+                    transformers=[
+                        ('cat', categorical_transformer, categorical_columns)],
+                        remainder='passthrough')
+
+            self.clf = Pipeline(steps=[('preprocessor', preprocessor),
+                                    ('classifier', self.clf)])
+
+            imputation_results = cross_val_score(
+                self.clf,
+                X,
+                y,
+                scoring=self.scoring,
+                cv=self.cv)
+
+            temp_results = {
+                    'strategy' : 'Model Imputation',
+                    'score': np.round(np.mean(imputation_results),3),
+                    'std':np.round(np.std(imputation_results),3),
+                }
+            results.append(temp_results)
+
+
+        self.results_df = pd.DataFrame(results)
+        self.results_df.sort_values(by='score',inplace=True)
+        self.fitted = True
+        return self
         
         
-    def compute(self):
+    def compute(self,return_scores=True):
         """
-        Compute class
+        Compute method.
+        """
+        self._check_if_fitted()
+        if return_scores :
+            return self.results_df
 
+    def fit_compute(self, X, y,column_names=None,class_names=None,categorical_columns='auto'):
         """
+        Calculates the cross validated results for various imputation strategies.
+        
+        Args:
+            X (pd.DataFrame):
+                input variables.
 
-    def fit_compute(self):
-        """
-        Fit & compute class
-        """
+            y (pd.Series):
+                target variable.
 
-    def show(self):
-        """
-        Show the results.
-        """
-        self._plot_results()
+            column_names (None, or list of str, optional):
+                List of feature names for the dataset. 
+                If None, then column names from the X dataframe are used.
 
-    def _get_no_imputer_scores(self,X,y):
-        """
-        Calculate the results without any imputation strategy.
-        Args :
-            X(pd.DataFrame) : Dataframe for X
-            y(pd.Series) : Target 
-        """
-        no_imputer_scores = cross_val_score(
-            self.clf,
-            X,
-            y,
-            scoring=self.scoring,
-            cv=self.cv)
-    
-        return no_imputer_scores
-
-    def _plot_results(self):
-        """
-        Plot the results.
-        """
-
-        imp_methods = []
-        performance = []
-        std_error = []
-        cmap=[]
-    
-        for k,v in self.results.items():
-            imp_methods.append(k)
-            performance.append(np.round(np.mean(v),4))
-            std_error.append(np.round(np.std(v),4))
-            cmap.append(np.random.rand(3,))
+            class_names (None, or list of str, optional):
+                List of class names e.g. ['neg', 'pos']. 
+                If none, the default ['Negative Class', 'Positive Class'] are
+                used.
             
+            categorical_features (None, or list of str, optional):default=auto
+                List of categorical features.The imputation strategy for categorical 
+                is different that compared to numerical features. If auto try to infer
+                the categorical columns based on 'object' and 'category' datatypes.
+        """
+        self.fit(X,y,
+        column_names=column_names,
+        class_names=class_names,
+        categorical_columns=categorical_columns
+        )
+        return self.compute()
 
-        y_pos = np.arange(len(imp_methods))    
 
-        plt.barh(y_pos, performance, xerr=std_error,align='center',color=cmap)
+    def plot(self,show=True):
+        """
+        Plot the results for imputation.
+        """
+        imp_methods = list(self.results_df['strategy'])
+        performance = list(self.results_df['score'])
+        std_error = list(self.results_df['std'])
+        y_pos = [i for i, _ in enumerate(imp_methods)]  
+        x_spacing = 0.01
+        y_spacing = 2*x_spacing
+        plt.barh(
+            y_pos, 
+            performance,
+            xerr=std_error,
+            align='center',
+            color=np.random.rand(len(performance),3))
+
         for index, value in enumerate(performance):
-            plt.text(value, index, str(value))
+            plt.text(value+x_spacing ,index+y_spacing, str(value),rotation=45)
         plt.yticks(y_pos, imp_methods)
         plt.xlabel('Metric')
         plt.title('Imputation Techniques')
-
-        plt.show()
-    
-
-   
+        plt.tight_layout()
+        if show:
+            plt.show()
+        else:
+            plt.close()
--- tests/missing/test_imputation.py
+++ tests/missing/test_imputation.py
@@ -14,31 +14,57 @@ from sklearn.preprocessing import KBinsDiscretizer
 import string
 import fire
 
-def test_imputation(choice=1):
+def test_imputation(choice=3):
    X,y = get_data(n_samples=1000,n_numerical=10,n_category=5)
    X_missing = generate_MCAR(X,missing=0.2)
+
+   strategies = {
+       'Simple Median Imputer' : SimpleImputer(strategy='median',add_indicator=True),
+       'Simple Mean Imputer' : SimpleImputer(strategy='mean',add_indicator=True),
+       'Iterative Imputer'  : IterativeImputer(add_indicator=True,n_nearest_features=5,
+       sample_posterior=True),
+       'KNN' : KNNImputer(n_neighbors=3),
+       'Random Imputer': RandomSampleImputer()
+
+   }
+
    #Initialize the classifier
    print(f'Using choice {choice}')
    if choice == 1:
        clf = RandomForestClassifier()
+       cmp = CompareImputationStrategies(
+           clf=clf,
+           strategies=strategies,
+           cv=5,
+           model_na_support=False)
    if choice == 2  :
        clf = xgb.XGBClassifier()
+       cmp = CompareImputationStrategies(
+           clf=clf,
+           strategies=strategies,
+           cv=5,
+           model_na_support=True)
    if choice == 3 :
        clf = lgb.LGBMClassifier()
+       cmp = CompareImputationStrategies(
+           clf=clf,
+           strategies=strategies,
+           cv=5,
+           model_na_support=True)
    if choice == 4 :
        clf = LogisticRegression()
+       cmp = CompareImputationStrategies(
+           clf=clf,
+           strategies=strategies,
+           cv=5,
+           model_na_support=False)
 
    #Create strategies for imputation.
-   strategies = {
-       'KNN' : KNNImputer(n_neighbors=3),
-       'Simple Median Imputer' : SimpleImputer(strategy='median',add_indicator=True),
-       'Iterative Imputer'  : IterativeImputer(add_indicator=True,n_nearest_features=5,
-       sample_posterior=True)
-
-   }
-   cmp = CompareImputationStrategies(clf=clf,strategies=strategies,cv=10)
-   cmp.fit(X_missing,y)
-   cmp.show()
+   
+  
+   cmp.fit_compute(X_missing,y)
+   cmp.plot()
+   
 
 
 def get_data(n_samples,n_numerical,n_category):
