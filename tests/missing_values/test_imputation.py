8b73b8ddeb073be5b50bebc2d21cdbd72b240b08
anilkumar.panda <panda.anil1989@gmail.com>
2021-03-15 09:10:35 +0100

    Running black on tests and removing ligm tests.

--- tests/missing_values/test_imputation.py
+++ tests/missing_values/test_imputation.py
@@ -1,90 +1,80 @@
-#Code to test the imputation strategies.
+# Code to test the imputation strategies.
 from probatus.missing_values.imputation import ImputationSelector
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.experimental import enable_iterative_imputer  
-from sklearn.impute import KNNImputer,SimpleImputer,IterativeImputer
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import KNNImputer, SimpleImputer, IterativeImputer
 import pandas as pd
 import numpy as np
 import pytest
-import os
-import lightgbm as lgb
 
-@pytest.fixture(scope='function')
+
+
+@pytest.fixture(scope="function")
 def X():
-    return pd.DataFrame({'col_1': [1, np.nan, 1, 1, np.nan, 1, 1, 0,1,1],
-                         'col_2': [0, 0, 0, np.nan, 0, 0, 0, 1,0,0],
-                         'col_3': [1, 0, np.nan, 0, 1, np.nan, 1, 0,1,1], 
-                         'col_4': ['A', 'B', 'A', np.nan, 'B', np.nan, 'C', 'A','B','C']}, index=[1, 2, 3, 4, 5, 6, 7, 8,9,10])
+    return pd.DataFrame(
+        {
+            "col_1": [1, np.nan, 1, 1, np.nan, 1, 1, 0, 1, 1],
+            "col_2": [0, 0, 0, np.nan, 0, 0, 0, 1, 0, 0],
+            "col_3": [1, 0, np.nan, 0, 1, np.nan, 1, 0, 1, 1],
+            "col_4": ["A", "B", "A", np.nan, "B", np.nan, "C", "A", "B", "C"],
+        },
+        index=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    )
 
-@pytest.fixture(scope='function')
+
+@pytest.fixture(scope="function")
 def y():
-    return pd.Series([1, 0, 1, 0, 1, 0, 1, 0,0,0], index=[1, 2, 3, 4, 5, 6, 7, 8,9,10])
+    return pd.Series(
+        [1, 0, 1, 0, 1, 0, 1, 0, 0, 0], index=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    )
 
-@pytest.fixture(scope='function')
+
+@pytest.fixture(scope="function")
 def strategies():
-    return  {
-        'Simple Median Imputer' : SimpleImputer(strategy='median',add_indicator=True),
-        'Simple Mean Imputer' : SimpleImputer(strategy='mean',add_indicator=True),
-        'Iterative Imputer'  : IterativeImputer(add_indicator=True,n_nearest_features=5,sample_posterior=True),
-        'KNN' : KNNImputer(n_neighbors=3),
-   }
+    return {
+        "Simple Median Imputer": SimpleImputer(strategy="median", add_indicator=True),
+        "Simple Mean Imputer": SimpleImputer(strategy="mean", add_indicator=True),
+        "Iterative Imputer": IterativeImputer(
+            add_indicator=True, n_nearest_features=5, sample_posterior=True
+        ),
+        "KNN": KNNImputer(n_neighbors=3),
+    }
 
 
-def test_imputation_linear(X,y,strategies,capsys):
-    
-   #Initialize the classifier
-   clf = LogisticRegression()
-   cmp = ImputationSelector(clf=clf,strategies=strategies,cv=3,model_na_support=False)
-   report = cmp.fit_compute(X,y)
-   cmp.plot(show=False)
-   
-   assert cmp.fitted == True
-   cmp._check_if_fitted()
-   assert report.shape[0]==4
+def test_imputation_linear(X, y, strategies, capsys):
 
-   # Check if there is any prints
-   out, _ = capsys.readouterr()
-   assert len(out) == 0
+    # Initialize the classifier
+    clf = LogisticRegression()
+    cmp = ImputationSelector(
+        clf=clf, strategies=strategies, cv=3, model_na_support=False
+    )
+    report = cmp.fit_compute(X, y)
+    cmp.plot(show=False)
+
+    assert cmp.fitted == True
+    cmp._check_if_fitted()
+    assert report.shape[0] == 4
+
+    # Check if there is any prints
+    out, _ = capsys.readouterr()
+    assert len(out) == 0
 
 
-def test_imputation_bagging(X,y,strategies,capsys):
+def test_imputation_bagging(X, y, strategies, capsys):
 
-   #Initialize the classifier
-   clf = RandomForestClassifier()
-   cmp = ImputationSelector(clf=clf,strategies=strategies,cv=3,model_na_support=False)
-   report = cmp.fit_compute(X,y)
-   cmp.plot(show=False)
-   
-   assert cmp.fitted == True
-   cmp._check_if_fitted()
-   assert report.shape[0]==4
+    # Initialize the classifier
+    clf = RandomForestClassifier()
+    cmp = ImputationSelector(
+        clf=clf, strategies=strategies, cv=3, model_na_support=False
+    )
+    report = cmp.fit_compute(X, y)
+    cmp.plot(show=False)
 
-   # Check if there is any prints
-   out, _ = capsys.readouterr()
-   assert len(out) == 0
+    assert cmp.fitted == True
+    cmp._check_if_fitted()
+    assert report.shape[0] == 4
 
-@pytest.mark.skipif(os.environ.get("SKIP_LIGHTGBM") == 'true', reason="LightGBM tests disabled")
-def test_imputation_boosting(X,y,capsys):
-    
-   #Create strategies for imputation.
-   strategies = {
-       'Simple Median Imputer' : SimpleImputer(strategy='median',add_indicator=True),
-       'Simple Mean Imputer' : SimpleImputer(strategy='mean',add_indicator=True),
-       'Iterative Imputer'  : IterativeImputer(add_indicator=True,n_nearest_features=5,
-       sample_posterior=True),
-       'KNN' : KNNImputer(n_neighbors=3),
-   }
-   #Initialize the classifier
-   clf = lgb.LGBMClassifier()
-   cmp = ImputationSelector(clf=clf,strategies=strategies,cv=3,model_na_support=True)
-   report = cmp.fit_compute(X,y)
-   cmp.plot(show=False)
-   
-   assert cmp.fitted == True
-   cmp._check_if_fitted()
-   assert report.shape[0]==5
-
-   # Check if there is any prints
-   out, _ = capsys.readouterr()
-   assert len(out) == 0
+    # Check if there is any prints
+    out, _ = capsys.readouterr()
+    assert len(out) == 0
