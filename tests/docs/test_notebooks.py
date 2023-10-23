98e3329af770fc4ca188d471b0237057328d00af
Adriano Oliveira <8104069+adri0@users.noreply.github.com>
2023-10-23 10:54:44 +0200

    Fix and reenable notebook tests (#235)
    - Refactor notebook tests to run all ipynb found in the `docs` folder.
    - Allows for exclusion of currently breaking notebooks.
    - Notebook tests will only run if the env variable `TEST_NOTEBOOKS` is
    set (which by default is not set).
    - Reenable notebooks tests in Github test workflow (by setting
    `TEST_NOTEBOOKS=1`).
    - Set a timeout (180 seconds) to which notebooks need to run.
    - Refactor the notebook
    `docs/tutorials/nb_automatic_best_num_features.ipynb` to run faster.
    - Fix compatibility issue on notebook
    `docs/tutorials/nb_imputation_comparison.ipynb` related to
    `IterativeImputer`.
    - Fix breaking SHAP dependency.
    
    ---------
    
    Co-authored-by: Adriano Oliveira <adriano.oliveira@ing.com>

--- tests/docs/test_notebooks.py
+++ tests/docs/test_notebooks.py
@@ -1,47 +1,32 @@
-# This approach is adapted from, and explained in: https://calmcode.io/docs/epic.html
-
 import os
+from pathlib import Path
 
 import pytest
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
 
-# Turn off interactive mode in plots
+# TODO Fix the breaking notebooks or make them run faster than timeout, then remove the fixed one from the list
+FAILING_NOTEBOOKS = {
+    "nb_shap_dependence.ipynb",
+    "nb_shap_variance_penalty_and_results_comparison.ipynb",
+    "nb_rfecv_vs_shaprfecv.ipynb",
+}
 
-plots_disable = "import matplotlib \n" "import matplotlib.pyplot as plt \n" "plt.ioff() \n" "matplotlib.use('Agg') \n"
-NOTEBOOKS_PATH = "./docs/tutorials/"
-
-NOTEBOOKS_TO_TEST_LGBM = [
-    "./docs/tutorials/nb_shap_feature_elimination",
-    "./docs/tutorials/nb_imputation_comparision",
-    "./docs/discussion/nb_rfecv_vs_shaprfecv.ipynb",
-]
-
-NOTEBOOKS_TO_TEST = [
-    "./docs/tutorials/nb_binning",
-    "./docs/tutorials/nb_custom_scoring",
-    "./docs/tutorials/nb_distribution_statistics",
-    "./docs/tutorials/nb_metric_volatility",
-    "./docs/tutorials/nb_sample_similarity",
-    "./docs/tutorials/nb_shap_model_interpreter" "./docs/howto/reproducibility",
-]
+TIMEOUT_SECONDS = 180
+PATH_NOTEBOOKS = [str(path) for path in Path("docs").glob("*/*.ipynb")]
+TEST_NOTEBOOKS = bool(os.environ.get("TEST_NOTEBOOKS"))  # Turn on tests by setting TEST_NOTEBOOKS = 1
+SKIP_ALL_NB_TESTS = not TEST_NOTEBOOKS
 
 
-def execute_notebook_test(notebook_name):
-    """
-    Execute a notebook.
-    """
-    notebook_path = notebook_name + ".ipynb"
+@pytest.mark.parametrize("notebook_path", PATH_NOTEBOOKS)
+@pytest.mark.skipif(SKIP_ALL_NB_TESTS, reason="Skip notebook tests if TEST_NOTEBOOK isn't set")
+def test_notebook(notebook_path: str) -> None:
+    """Run a notebook and check no exception is raised."""
+    if Path(notebook_path).name in FAILING_NOTEBOOKS:
+        pytest.skip(f"NEEDS FIXING! - Notebook {notebook_path} is either failing or taking too long to run.")
 
-    code_to_execute = os.popen(
-        f"jupyter nbconvert --to script --execute --stdout --PythonExporter.exclude_markdown=True {notebook_path}"
-    ).read()
-    _ = os.popen(f"python3 -c {plots_disable + code_to_execute}").read()
+    with open(notebook_path) as f:
+        nb = nbformat.read(f, as_version=4)
 
-
-@pytest.mark.parametrize("notebook_name", NOTEBOOKS_TO_TEST_LGBM)
-@pytest.mark.skipif(os.environ.get("SKIP_LIGHTGBM") == "true", reason="LightGBM tests disabled")
-@pytest.mark.skip(reason="GitHub pipelines are failing on these tests. Could not find a solution for now.")
-def test_jupyter_notebook_lgbm(notebook_name):
-    """
-    Test a notebook.
-    """
-    execute_notebook_test(notebook_name)
+    ep = ExecutePreprocessor(timeout=TIMEOUT_SECONDS, kernel_name="python3")
+    ep.preprocess(nb, {"metadata": {"path": str(Path(notebook_path).parent)}})
