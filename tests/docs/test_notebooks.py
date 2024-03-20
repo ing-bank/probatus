0be6f3fa58bb5dff2d7a0c5761d9ed60cdc93230
Reinier Koops <info@reinier.work>
2024-03-20 20:18:19 +0100

    fix nb run flag

--- tests/docs/test_notebooks.py
+++ tests/docs/test_notebooks.py
@@ -7,12 +7,12 @@ from nbconvert.preprocessors import ExecutePreprocessor
 
 TIMEOUT_SECONDS = 1800
 PATH_NOTEBOOKS = [str(path) for path in Path("docs").glob("*/*.ipynb")]
-TEST_NOTEBOOKS = bool(os.environ.get("TEST_NOTEBOOKS"))  # Turn on tests by setting TEST_NOTEBOOKS = 1
-SKIP_ALL_NB_TESTS = not TEST_NOTEBOOKS
+NB_FLAG = os.environ.get("TEST_NOTEBOOKS")  # Turn on tests by setting TEST_NOTEBOOKS = 1
+TEST_NOTEBOOKS = False if NB_FLAG == "1" else True
 
 
 @pytest.mark.parametrize("notebook_path", PATH_NOTEBOOKS)
-@pytest.mark.skipif(SKIP_ALL_NB_TESTS, reason="Skip notebook tests if TEST_NOTEBOOK isn't set")
+@pytest.mark.skipif(TEST_NOTEBOOKS, reason="Skip notebook tests if TEST_NOTEBOOK isn't set")
 def test_notebook(notebook_path: str) -> None:
     """Run a notebook and check no exception is raised."""
     with open(notebook_path) as f:
