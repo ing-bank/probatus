0fdc7466e297a7f36509ced1da03cabbd68bc083
Reinier Koops <info@reinier.work>
2024-03-22 20:12:02 +0100

    speed up (only for big use-cases with many features

--- probatus/feature_elimination/__init__.py
+++ probatus/feature_elimination/__init__.py
@@ -1,3 +1,4 @@
-from .feature_elimination import ShapRFECV, EarlyStoppingShapRFECV
+from .feature_elimination import ShapRFECV
+from .early_stopping_feature_elimination import EarlyStoppingShapRFECV
 
 __all__ = ["ShapRFECV", "EarlyStoppingShapRFECV"]
